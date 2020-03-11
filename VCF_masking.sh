233cc7be417e0aa125d619212af922cbf9e6170e
Chris Wheat <chriswheat@zoologi116.zoologi.su.se>
2020-03-11 12:14:42 +0100

    updates

--- VCF_masking.sh
+++ VCF_masking.sh
@@ -12,7 +12,8 @@ AbiskoPn1900.sorted.bam  Dalarna54.8.sorted.bam  Gotland41.6.sorted.bam  SkanePn
 parallel "bedtools genomecov -ibam {} -bg  > {.}.bed" ::: *.sorted.bam
 
 # filter these for having depth > 3
-parallel "awk '$4 > 3' {} > {.}.gt3.bed" ::: *.bed
+my_awk='$4 > 3'
+parallel "awk '$my_awk' {} > {.}.gt3.bed" ::: *.sorted.bed
 
 # combine all, then sort
 cat *.gt3.bed | sort -k1,1 -k2,2n > all.sorted_gt3.bed
@@ -49,11 +50,12 @@ Chromosome_25   4833285
 bedtools merge -i all.sorted_gt3.bed | bedtools complement -i - -g chromo_sizes.tsv > all.sorted_gt3.merged.readdepth_lt_4.bed
 
 
-# # make N regions of the genome
+# make mask of the N regions of the genome
 # https://www.danielecook.com/generate-a-bedfile-of-masked-ranges-a-fasta-file/
 # ln -s /cerberus/projects/shared_napi_rapae/assemblies/Pieris_napi_fullAsm_chomoOnly.fasta Pieris_napi_fullAsm_chomoOnly.fa
 # python generate_masked_ranges.py Pieris_napi_fullAsm_chomoOnly.fa Chromosome_ Chromosome_ > Pieris_napi_fullAsm_chomoOnly.Nmask.bed
 
+# using this result now
 genome_Ns=/cerberus/projects/chrwhe/Pieris_napi_old_demography/Pieris_napi_fullAsm_chomoOnly.Nmask.bed
 
 # get the repeat file for the reference
