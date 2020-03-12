69fe46b9e208c3704e5ffa7ea56b355ef8ec68ae
Chris Wheat <chriswheat@zoologi116.zoologi.su.se>
2020-03-12 14:49:55 +0100

    alt version

--- smcpp_run.sh
+++ smcpp_run.sh
@@ -18,12 +18,18 @@ for i in {1..4} ; do echo $i ; done | cat | parallel -j 3 "smc++ cv --timepoints
 # smc++ plot -g 0.5 pop1_g0.5.pdf pop1/model.final.json
 # for g 1 plots
 # for i in {1..4} ; do echo $i ; done | cat | parallel -j 3 "smc++ plot -g 1 pop{}.pdf pop{}/model.final.json"
+# all together in one plot
+smc++ plot -g 1 combined_time_specified.pdf pop[1-4]/model.final.json
 
 # without timepoints specified
 # for g 0.5 plot
 smc++ plot -g 0.5 pop1_g0.5_notime_specified.pdf pop1_notime_specified/model.final.json
-# for g 1 plots
-for i in {1..4} ; do echo $i ; done | cat | parallel -j 3 "smc++ plot -g 1 pop{}_notime_specified.pdf pop{}_notime_specified/model.final.json"
+
+# for g 1 plots separately
+# for i in {1..4} ; do echo $i ; done | cat | parallel -j 3 "smc++ plot -g 1 pop{}_notime_specified.pdf pop{}_notime_specified/model.final.json"
+
+# all together in one plot
+smc++ plot -g 1 combined_notime_specified.pdf pop*_notime_specified/model.final.json
 
 ################################################
 # email when done
